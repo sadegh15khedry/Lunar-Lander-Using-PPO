f3234c272d43aa5b71dad1fad606abdf2978db36
sadegh <sadegh15khedry@gmail.com>
2024-06-20 07:45:46 +0330

    PPO algrorithm

--- lunar_lander_deepQ.py
+++ lunar_lander_deepQ.py
@@ -1,57 +1,61 @@
-import gym
-from gym.envs import box2d
-import numpy as np
-from collections import deque
-import random
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.evaluation import evaluate_policy
 import gymnasium as gym
 
-#setting up gymnasium
-env = gym.make("LunarLander-v2", render_mode="human")
-# observation, info = env.reset(seed=42)
-# for _ in range(1000):
-#    action = env.action_space.sample()  # this is where you would insert your policy
-#    observation, reward, terminated, truncated, info = env.step(action)
+LOAD = 0
 
-#    if terminated or truncated:
-#       observation, info = env.reset()
-episodes = 10
-for episode in range(0, episodes):
-   state = env.reset()
-   done = False
-   score = 0
-   
-   while not done:
-      env.render()
-      action = env.action_space.sample()
-      # n_state, reward, done, info = env.step(action)
-      obs, reward, done, truncated, info = env.step(action)
-      score += reward
-      
-   print(f"Episode: {episode}  Score: {score}")
+
+#setting up our environment
+env = gym.make("LunarLander-v2", render_mode="human")
+env = DummyVecEnv([lambda: env])
+
+#setting up the model
+model = PPO("MlpPolicy", env, verbose = 1)
+# if (LOAD == 1)
+#     model = sb3.PPO.load("ppo_model.zip")
+
+#training the model
+model.learn(total_timesteps=100000)
+
+#evalutating our model
+evaluate_policy(model, env,  n_eval_episodes=10, render=True)
+
+#Saving the model
+model.save("ppo_model.zip")
+
+#closing the environment
 env.close()
 
 
 
-
-
-# # Set up the environment
-# env = gym.make("LunarLander-v2", render_mode= "human")
+##Random aciton implementation
+# import numpy as np
+# from collections import deque
+# import random
 
 # episodes = 10
-# for episode in range(1, episodes+1):
-#   state = env.reset()
-#   done = False 
-#   score = 0
-
-#   while not done:
-#     action = random.choice([0, 1, 2])
-#     _, reward, done, _ = env.step(action)
-#     score += reward
-
-#   print(f"episode: {episode} , score: {score}")
-
+# for episode in range(0, episodes):
+#    state = env.reset()
+#    done = False
+#    score = 0
+   
+#    while not done:
+#       env.render()
+#       action = env.action_space.sample()
+#       obs, reward, done, truncated, info = env.step(action)
+#       score += reward
+      
+#    print(f"Episode: {episode}  Score: {score}")
 # env.close()
+
+
+
+
+
 
