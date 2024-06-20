d282cf6d7bdbf55060f33d83712c8f1af12518c0
sadegh <sadegh15khedry@gmail.com>
2024-06-20 08:29:15 +0330

    added the ppo model zip file

--- random_aciton_lunar_lander.py
+++ random_aciton_lunar_lander.py
@@ -7,38 +7,25 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 import gymnasium as gym
-
-LOAD = 0
-
+import numpy as np
+from collections import deque
+import random
 
 #setting up our environment
 env = gym.make("LunarLander-v2", render_mode="human")
 env = DummyVecEnv([lambda: env])
 
-#setting up the model
-model = PPO("MlpPolicy", env, verbose = 1)
-# Uncomment the following lines to load a pre-trained model
-# if (LOAD == 1)
-#     model = sb3.PPO.load("ppo_model.zip")
-
-#training the model
-model.learn(total_timesteps=100000)
-
-#evalutating our model
-evaluate_policy(model, env,  n_eval_episodes=10, render=True)
-
-#Saving the model
-model.save("ppo_model.zip")
-
-#closing the environment
+episodes = 10
+for episode in range(0, episodes):
+   state = env.reset()
+   done = False
+   score = 0
+   
+   while not done:
+      env.render()
+      action = env.action_space.sample()
+      obs, reward, done, truncated, info = env.step(action)
+      score += reward
+      
+   print(f"Episode: {episode}  Score: {score}")
 env.close()
-
-
-
-
-
-
-
-
-
-
